6152d01f4493820a24be114a45e6aeeaa30fe368
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2023-09-09 17:02:21 +0530

    Added more components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Box} from '@mui/material';
+import HeroBanner from './HeroBanner'
+import SearchExer from './SearchExer';
+import Exercises from './Exercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+        <HeroBanner/>
+        <SearchExer/>
+        <Exercises/>
+    </Box>
   )
 }
 
---
+++ src/components/SearchExer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchExer = () => {
+  return (
+    <div>SearchExer</div>
+  )
+}
+
+export default SearchExer
